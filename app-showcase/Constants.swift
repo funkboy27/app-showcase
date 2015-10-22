5dcf11635d9be04386c5f055ea6e65eebb39c6c5
Matthew Wells <matthew@Matthews-Mac-mini.local>
2015-10-22 01:57:10 -0700

    setup login screen

---
+++ app-showcase/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  app-showcase
+//
+//  Created by Matthew Wells on 2015-10-22.
+//  Copyright © 2015 Matthew Wells. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
---
+++ app-showcase/MaterialTextField.swift
@@ -0,0 +1,28 @@
+//
+//  MaterialTextField.swift
+//  app-showcase
+//
+//  Created by Matthew Wells on 2015-10-22.
+//  Copyright © 2015 Matthew Wells. All rights reserved.
+//
+
+import UIKit
+
+class MaterialTextField: UITextField {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = 2.0
+        layer.borderColor = UIColor(red: SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.1).CGColor
+        layer.borderWidth = 1.0
+    }
+    
+    // For placeholder text
+    override func textRectForBounds(bounds: CGRect) -> CGRect {
+        return CGRectInset(bounds, 10, 0)
+    }
+    
+    //for editable text
+    override func editingRectForBounds(bounds: CGRect) -> CGRect {
+        return CGRectInset(bounds, 10, 0)
+    }
+}
