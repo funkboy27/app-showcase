1ca5d9f0d5404273ac90ea7f878ab702da6646a2
Matt Wells <mattwells24@hotmail.com>
2015-10-22 01:10:50 -0700

    Initial Commit

---
+++ app-showcase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app-showcase
+//
+//  Created by Matthew Wells on 2015-10-22.
+//  Copyright © 2015 Matthew Wells. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
